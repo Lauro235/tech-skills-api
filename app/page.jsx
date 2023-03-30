f58d8a876b48e7bafc1b3dbd587152426ac22c68
Lorentz <91217473+Lauro235@users.noreply.github.com>
2023-03-30 15:44:14 +0100

    change text color

--- app/page.jsx
+++ app/page.jsx
@@ -86,7 +86,7 @@ function Home() {
               type="text"
               name="job-title"
               id="job-title"
-              className="w-full px-4 py-2 rounded-md border border-gray-400 focus:border-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-600"
+              className="w-full px-4 py-2 rounded-md border border-gray-400 focus:border-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-600 text-black"
               placeholder="Enter your job title"
               onChange={handleChangeInput}
             />
