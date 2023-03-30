a1caad78d620be70cb67834b6a43e8a74a9e89c8
Callum <84125856+callum-cheshire@users.noreply.github.com>
2023-03-30 11:42:24 +0100

    separated data concerns

--- app/page.jsx
+++ app/page.jsx
@@ -1,61 +1,8 @@
+import { candidates, requiredRoles, contract } from "@/data/data";
+
 export default function Home() {
   // Define the contract object
-  const contract = {
-    startDate: "2023-04-01",
-    endDate: "2023-09-30",
-    name: "John Doe",
-    contact: "Software Engineer",
-    skills: {
-      Java: 5,
-      Javascript: 5,
-    },
-  };
-  // Define the required roles object
-  const requiredRoles = {
-    "Java Developer": {
-      Java: 5,
-      AWS: 2,
-    },
-    "Javascript Developer": {
-      Javascript: 5,
-      AWS: 1,
-    },
-    "Python Developer": {
-      Python: 5,
-      AWS: 1,
-    },
-  };
-  // Define a sample list of candidates
-  const candidates = [
-    {
-      name: "Alice",
-      skills: {
-        Java: 5,
-        AWS: 3,
-      },
-    },
-    {
-      name: "Bob",
-      skills: {
-        Javascript: 5,
-        AWS: 2,
-      },
-    },
-    {
-      name: "Charlie",
-      skills: {
-        Python: 4,
-        AWS: 1,
-      },
-    },
-    {
-      name: "David",
-      skills: {
-        Java: 4,
-        AWS: 2,
-      },
-    },
-  ];
+
   // Define the matching algorithm
   function matchRoles(requiredRoles, candidates) {
     const matchedCandidates = {};
