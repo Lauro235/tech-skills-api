695685072f27bb982d339b5fb0844bc39dc40830
Julio Velezmoro <julio28ve@gmail.com>
2023-03-30 11:31:48 +0100

    form updated MVP

--- app/page.js
+++ app/page.js
@@ -110,6 +110,29 @@ export default function Home() {
               </div>
             </div>
             <div className="sm:col-span-4">
+              <h2 className="text-base font-semibold leading-7 text-gray-900">
+                Skills
+              </h2>
+              <label
+                htmlFor="skills"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Java
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <input
+                    type="checkbox"
+                    name="skills"
+                    id="skills"
+                    autoComplete="job title"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+            </div>
+
+            {/* <div className="sm:col-span-4">
               <label
                 htmlFor="email"
                 className="block text-sm font-medium leading-6 text-gray-900"
@@ -128,9 +151,8 @@ export default function Home() {
                   />
                 </div>
               </div>
-            </div>
-
-            <div className="sm:col-span-4">
+            </div> */}
+            {/*<div className="sm:col-span-4">
               <label
                 htmlFor="start date"
                 className="block text-sm font-medium leading-6 text-gray-900"
@@ -161,12 +183,9 @@ export default function Home() {
                   className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"
                 />
               </div>
-            </div>
-
+            </div> */}
           </div>
         </div>
-
-        
       </div>
 
       <div className="mt-6 flex items-center justify-start gap-x-6">
